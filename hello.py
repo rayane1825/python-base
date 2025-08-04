e1c954a08e45d17cfd4ec67dacbbdb11243dc4e2
Rayane Alves <raypaz1825@gmail.com>
2025-08-04 17:22:51 +0000

    Adicionando .gitignore e modificações em hello.py

--- hello.py
+++ hello.py
@@ -27,13 +27,13 @@ current_language = os.getenv("LANG","en_US")[:5]
 msg = "Hello, World!"
 
 if current_language == "pt_BR":
-    msg = "Olá, Mundo!"
+    msg = "Olá,Mundo!"
 elif current_language == "it_IT":
-    msg = "Ciao, Mondo!"
+    msg = "Ciao,Mondo!"
 elif current_language == "es_SP":
-    msg = "Hola, Mundo!"
+    msg = "Hola,Mundo!"
 elif current_language == "fr_FR":
-    msg = "Bonjour, Monde!" 
+    msg = "Bonjour,Monde!" 
 
 
 
